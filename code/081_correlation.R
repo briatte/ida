ab3e765e383238df96041ba63f0f02b9d6cbf648
François <briatte@gmail.com>
2013-03-15 00:57:47 +0100

    spelling, sorry MG

--- code/081_correlation.R
+++ code/081_correlation.R
@@ -8,30 +8,30 @@ source <- "https://raw.github.com/jeromyanglim/oecd_life_analysis/master/oecd-li
 if(!file.exists(file <- "data/oecd-life.csv"))
   download(source, file, mode = "wb")
 # Import the CSV file.
-oecd.d <- read.csv(file, sep = "\t", stringsAsFactors = FALSE)[1:36, ]
+oecd <- read.csv(file, sep = "\t", stringsAsFactors = FALSE)[1:36, ]
 
 
 
 # Extract numeric data.
-oecd.d[-1] <- as.numeric(gsub("[^0-9.]", "", as.matrix(oecd.d[-1])))
+oecd[-1] <- as.numeric(gsub("[^0-9.]", "", as.matrix(oecd[-1])))
 
 
 
 # Load countrycode package.
 require(countrycode)
 # Add country codes.
-oecd.d$iso3c <- countrycode(oecd.d$COUNTRY, "country.name", "iso3c")
+oecd$iso3c <- countrycode(oecd$COUNTRY, "country.name", "iso3c")
 # Check result.
-head(oecd.d)[c(1, 4, 6, 13, 26)]
+head(oecd)[c(1, 4, 6, 13, 26)]
 
 
 
 # Compute average life expectancy.
-ymean <- mean(oecd.d$Life.expectancy)
+ymean <- mean(oecd$Life.expectancy)
 # Compute average water quality.
-xmean <- mean(oecd.d$Water.quality)
+xmean <- mean(oecd$Water.quality)
 # Plot both variables with country codes as data points.
-g <- qplot(data = oecd.d, y = Life.expectancy, x = Water.quality, 
+g <- qplot(data = oecd, y = Life.expectancy, x = Water.quality, 
       label = iso3c, geom = "text") +
   geom_vline(x = xmean, linetype = "dashed") +
   geom_hline(y = ymean, linetype = "dashed")
@@ -41,8 +41,8 @@ g
 
 
 # Create a dummy denoting positive or negative correlation.
-bottom.left <- (oecd.d$Water.quality < xmean) & (oecd.d$Life.expectancy < ymean)
-top.right   <- (oecd.d$Water.quality > xmean) & (oecd.d$Life.expectancy > ymean)
+bottom.left <- (oecd$Water.quality < xmean) & (oecd$Life.expectancy < ymean)
+top.right   <- (oecd$Water.quality > xmean) & (oecd$Life.expectancy > ymean)
 correlation <- ifelse(bottom.left | top.right, "positive", "negative")
 # Add colored circles to discriminate the data points.
 g + geom_point(size=16, alpha = .4, aes(color = correlation)) +
@@ -57,31 +57,31 @@ g + geom_smooth(fill = "green", color = "forestgreen", alpha = .2)
 
 
 # Correlation coefficient.
-with(oecd.d, cor(Life.expectancy, Water.quality))
+with(oecd, cor(Life.expectancy, Water.quality))
 
 
 
 # Load arm package.
 require(arm)
 # Absolute correlation plot.
-corrplot(oecd.d[-c(1, 26)], color = TRUE)
+corrplot(oecd[-c(1, 26)], color = TRUE)
 
 
 
 # Compute correlation coefficient.
-with(oecd.d, cor(Life.Satisfaction, Long.term.unemployment.rate, use = "complete.obs"))
+with(oecd, cor(Life.Satisfaction, Long.term.unemployment.rate, use = "complete.obs"))
 # Plot with smoothed trend.
-qplot(data = oecd.d, y = Life.Satisfaction, x = Long.term.unemployment.rate,
+qplot(data = oecd, y = Life.Satisfaction, x = Long.term.unemployment.rate,
       label = iso3c, geom = "text") +
   geom_smooth(fill = "red", color = "darkred", alpha = .2)
 
 
 
 # Scatterplot matrix.
-pairs(oecd.d[c(15:17, 25)])
+pairs(oecd[c(15:17, 25)])
 # Load car package.
 require(car)
 # More sophisticated output.
-scatterplotMatrix(oecd.d[c(15:17, 25)], smoother=NULL)
+scatterplotMatrix(oecd[c(15:17, 25)], smoother=NULL)
 
 
--- code/082_ols.R
+++ code/082_ols.R
@@ -1,6 +1,24 @@
 
 
-library(foreign)
+# Load packages.
+require(downloader)
+require(foreign)
+dataset = "data/hibbs.2012.txt"
+# Download the data.
+if(!file.exists(dataset)) {
+  file = "data/hibbs.2012.dta"
+  if(!file.exists(file))
+    download("http://www.douglas-hibbs.com/HibbsArticles/HIBBS-OBAMA-FORECAST-27July2012-WEBPOST.dta", file)
+  hibbs <- read.dta(file)
+  write.table(hibbs, dataset)
+}
+# Load the data.
+hibbs <- read.table(dataset, stringsAsFactors=FALSE)
+# Subset to election quarters.
+hibbs <- subset(hibbs, !is.na(presvote))
+
+
+
 source("code/vwreg.r")
 
 
@@ -17,11 +35,7 @@ if (file.exists("data/qog_basic_cs.csv")) {
   write.csv(qog, file = "data/qog_basic_cs.csv")
 }
 
-
-
 p1 <- vwReg(wdi_fr ~ bl_asyf25 + I(bl_asyf25^2), qog, method=lm)
-
-
 
 p1 + ylab("Fertility rate") + xlab("Average education years among 25+ year-old females")
 
